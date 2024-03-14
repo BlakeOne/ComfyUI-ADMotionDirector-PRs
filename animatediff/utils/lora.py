4a2edaedbe1c300def73231d6676ddcedbdf4033
BlakeOne <30273164+BlakeOne@users.noreply.github.com>
2024-03-13 19:45:14 -0700

    x

--- animatediff/utils/lora.py
+++ animatediff/utils/lora.py
@@ -47,23 +47,25 @@ class LoraInjectedLinear(nn.Module):
         self.r = r
         self.linear = nn.Linear(in_features, out_features, bias)
         self.lora_down = nn.Linear(in_features, r, bias=False)
-        self.dropout = nn.Dropout(dropout_p)
+        self.lora_mid = nn.Linear(r, r, bias=False, device=device, dtype=dtype)        
         self.lora_up = nn.Linear(r, out_features, bias=False)
+        self.dropout = nn.Dropout(dropout_p)
         self.scale = scale
         self.selector = nn.Identity()
 
         nn.init.normal_(self.lora_down.weight, std=1 / r)
+        nn.init.zeros_(self.lora_mid.weight)
         nn.init.zeros_(self.lora_up.weight)
 
     def forward(self, input):
         return (
             self.linear(input)
-            + self.dropout(self.lora_up(self.selector(self.lora_down(input))))
+            + self.dropout(self.lora_up(self.selector(self.lora_mid(self.lora_down(input)))))
             * self.scale
         )
 
     def realize_as_lora(self):
-        return self.lora_up.weight.data * self.scale, self.lora_down.weight.data
+        return self.lora_up.weight.data * self.scale, self.lora_mid.weight.data, self.lora_down.weight.data
 
     def set_selector_from_diag(self, diag: torch.Tensor):
         # diag is a 1D tensor of size (r,)
